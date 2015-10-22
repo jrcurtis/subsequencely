d5c64686cf23c5d324d2b1a3652d150af9ff7942
Jacob <muskratwaltz@gmail.com>
2015-10-22 12:38:05 -0700

    Better bipolar slider support.

--- src/slider.c
+++ src/slider.c
@@ -45,10 +45,24 @@ u8 slider_handle_press(Slider* s, u8 index, u8 value)
         return 0;
     }
 
+    // All the pads before the one that was pressed are filled up, so find
+    // their value, then find how much of the pressed pad is filled up based
+    // on how hard it was pressed.
     s->value = pad * s->resolution;
 
-    u8 bucket_size = 128 / s->resolution;
-    s->value += (value + bucket_size) / bucket_size;
+    // The harder you press, the further away from 0 the value should be,
+    // so if there's a negative offset and the pad that was pressed is on
+    // the negative side of 0, then reverse the velocity.
+    if (s->value + s->offset < 0)
+    {
+        value = 127 - value;
+    }
+
+    // Each pad has a number of possible values, determined by s->resolution,
+    // so figure out what fraction of them are filled and make sure at least
+    // one is filled, so that the value doesn't creep into the range of the
+    // previous pad.
+    s->value += max(1, s->resolution * value / 127);
 
     return 1;
 }
@@ -70,22 +84,55 @@ void slider_draw(Slider* s)
         coord = &x;
     }
 
+    // For positive offset (unipolar) sliders, all the pads before the value
+    // are filled, but for negative offsets, only the pads between the one
+    // that 0 is in and the one the value lies in are filled.
+    u8 range_start = 0;
+    u8 range_end = 0;
+    u8 zero_point = -s->offset / s->resolution - 1;
+    u8 value_point = (s->value - 1) / s->resolution;
+
+    if (s->offset >= 0)
+    {
+        range_end = value_point;
+    }
+    else if (s->value + s->offset <= 0)
+    {
+        range_start = value_point;
+        range_end = zero_point;
+    }
+    else 
+    {
+        range_start = zero_point;
+        range_end = value_point;
+    }
+
     u8 prev_value = 0;
 
     for (*coord = 0; *coord < GRID_SIZE; (*coord)++)
     {
         const u8* color = off_color;
-        u8 dimness = 0;
-
-        if (prev_value < s->value)
+        if (*coord >= range_start
+            && *coord <= range_end)
         {
             color = s->color;
         }
 
-        u8 diff = s->value - prev_value;
-        if (diff < s->resolution)
+        u8 dimness = 0;
+        if (*coord == value_point)
         {
-            dimness = 5 * (s->resolution - diff) / s->resolution;
+            // Figure out how much of the resolution of this pad is filled.
+            // Reverse the direction for negative values just like in
+            // handle_press.
+            u8 pad_value = s->value - prev_value;
+            if (s->value + s->offset <= 0)
+            {
+                pad_value = s->resolution - pad_value + 1;
+            }
+
+            // Fit the different possible resolutions into 5 brightness levels,
+            // so that the lights can't go all the way off.
+            dimness = 5 * (s->resolution - pad_value) / s->resolution;
         }
 
         plot_pad_dim(coord_to_index(x, y), color, dimness);
